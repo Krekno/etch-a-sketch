2b9fdf9405be01161ebf20849946553be830119a
Krekno <yigiterenkarabas@gmail.com>
2024-02-16 01:29:58 +0300

    changed the number of divs in container to 256, divs are in a 16 column grid display

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const container = document.querySelector('#container')
 
-for (let i = 0; i < 64; i++) {
+for (let i = 0; i < 256; i++) {
     var div = document.createElement("div");
     div.className = "square";
+    div.textContent = "a";
     container.appendChild(div);
 }
