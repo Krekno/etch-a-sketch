63d56840856d3e1a9624d1c9e9eeb8bd4ccd8c79
Krekno <yigiterenkarabas@gmail.com>
2024-03-14 19:50:06 +0300

    changed spaces to tabs

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const container = document.querySelector('#container')
 
-for (let i = 0; i < 256; i++) {
-    var div = document.createElement("div");
-    div.className = "square";
-    div.textContent = "a";
-    container.appendChild(div);
+for (let i = 0; i < 16; i++) {
+	for (let j = 0; j < 16; j++) {
+		const div = document.createElement('div')
+		div.classList.add('square')
+		container.appendChild(div)
+	}
 }
+
+const square = document.querySelector('.square')
+
+square.addEventListener('mouseover', () => {
+	square.style.backgroundColor = "red";
+});
