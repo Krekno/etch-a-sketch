332447a8fbd014cefaf7299e880b5fe182bceeab
Krekno <yigiterenkarabas@gmail.com>
2024-05-09 19:51:54 +0300

    can change the size of canvas now

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const container = document.querySelector('#container')
 
-for (let i = 0; i < 16; i++) {
-	for (let j = 0; j < 16; j++) {
+x = prompt("Enter the number of columns")
+y = prompt("Enter the number of rows")
+
+container.style.gridTemplateColumns = `repeat(${x}, 50px)`
+
+for (let i = 0; i < y; i++) {
+	for (let j = 0; j < x; j++) {
 		const div = document.createElement('div')
 		div.classList.add('square')
 		container.appendChild(div)
@@ -9,17 +14,14 @@ for (let i = 0; i < 16; i++) {
 }
 
 const squares = document.querySelectorAll('.square');
-
 squares.forEach(square => {
 	square.addEventListener('mouseover', () => {
 		square.style.backgroundColor = "red";
 	});
 });
-
 const resetButton = document.createElement('button');
 resetButton.textContent = 'Reset';
 container.appendChild(resetButton);
-
 resetButton.addEventListener('click', () => {
 	squares.forEach(square => {
 		square.style.backgroundColor = "black";
